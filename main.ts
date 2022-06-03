45e56d6b0f106b6916c8d4d4fce8a3b471756496
Josip Kalebic <josip.kalebic@gmail.com>
2022-06-03 19:29:46 +0200

    Remove RF transmission, change probe temp error handling

--- main.ts
+++ main.ts
@@ -6,9 +6,28 @@ let t1 = 0
 let temp2 = 0
 let hum2 = 0
 let air = 0
-radio.setGroup(15)
-radio.setTransmitPower(4)
-loops.everyInterval(1000, function () {
+serial.setBaudRate(BaudRate.BaudRate115200)
+loops.everyInterval(600, function () {
+    air = pins.analogReadPin(AnalogPin.P1)
+})
+basic.forever(function () {
+    t1_error = 0
+    t1 = dstemp.celsius(DigitalPin.P16)
+    if (t1_error == 0) {
+        t1 = 0
+    }
+    serial.writeString("0,")
+    serial.writeNumber(t1)
+    serial.writeString(",")
+    serial.writeNumber(temp2)
+    serial.writeString(",")
+    serial.writeNumber(hum2)
+    serial.writeString(",")
+    serial.writeNumber(air)
+    serial.writeLine(",")
+    basic.pause(200)
+})
+loops.everyInterval(700, function () {
     dht11_dht22.queryData(
     DHTtype.DHT11,
     DigitalPin.P8,
@@ -19,30 +38,5 @@ loops.everyInterval(1000, function () {
     if (dht11_dht22.readDataSuccessful()) {
         temp2 = dht11_dht22.readData(dataType.temperature)
         hum2 = dht11_dht22.readData(dataType.humidity)
-    }
-})
-loops.everyInterval(1000, function () {
-    air = pins.analogReadPin(AnalogPin.P1)
-})
-loops.everyInterval(2000, function () {
-    if (t1_error == 0) {
-        radio.sendValue("temp", t1)
-        basic.pause(100)
-    }
-    radio.sendValue("air", air)
-})
-basic.forever(function () {
-    t1_error = 0
-    t1 = dstemp.celsius(DigitalPin.P16)
-    if (t1_error == 0) {
-        serial.writeString("0,")
-        serial.writeNumber(t1)
-        serial.writeString(",")
-        serial.writeNumber(temp2)
-        serial.writeString(",")
-        serial.writeNumber(hum2)
-        serial.writeString(",")
-        serial.writeNumber(air)
-        serial.writeLine(",")
     }
 })
